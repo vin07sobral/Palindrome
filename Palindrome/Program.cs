313f7d0d1f16c6137baa10ca75238750e7d6a61e
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-11-21 17:16:32 -0200

    Solução Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = string.Empty;
+
+            Console.WriteLine("Insira sua palavra: ");
+            a = Console.ReadLine();
+
+            char[] temp = a.ToCharArray();
+            Array.Reverse(temp);
+            string b = new string(temp);
+
+            if (a.Equals(b))
+            {
+                Console.WriteLine("É Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Não é palindrome");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
